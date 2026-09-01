55d63ed4aaa3bc92130e98d3e40fff8415fb6298
pedestriantactics <pedestriantactics@gmail.com>
2026-09-01 18:58:35 -0400

    new config files

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,28 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-import { mdsvex } from 'mdsvex'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md'],
-}
+    extensions: ['.md'],
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html', // Required for client-side routing on reload
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			// If deploying via GitHub Actions, it sets GITHUB_REPOSITORY (e.g., 'user/repo')
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/photos'
-		}
-	}
-}
+    extensions: ['.svelte', '.md'],
+    preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/photos'
+        }
+    }
+};
 
 export default config;
