9517fa6ea0080e1390df5b39e335b082c9ac650d
pedestriantactics <pedestriantactics@gmail.com>
2026-09-01 19:17:31 -0400

    edits

--- svelte.config.js
+++ svelte.config.js
@@ -12,14 +12,8 @@ const mdsvexOptions = {
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-    }),
+			fallback: '404.html'
+		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
