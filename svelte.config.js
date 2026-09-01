e9587404ba56ff63e10e4bcff6d9ce518569a67d
pedestriantactics <pedestriantactics@gmail.com>
2026-09-01 19:11:41 -0400

    testing base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,29 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-    extensions: ['.md'],
-};
+	extensions: ['.md'],
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    extensions: ['.svelte', '.md'],
-    preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: '404.html',
-            precompress: false,
-            strict: true
-        }),
-        paths: {
-            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/photos'
-        }
-    }
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+    }),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.photos
+		}
+	}
 };
 
 export default config;
