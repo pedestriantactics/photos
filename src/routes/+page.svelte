2c6042afac9bdb8bf3508d0c4ea3d1d90974de11
pedestriantactics <pedestriantactics@gmail.com>
2026-09-01 22:33:40 -0400

    yeah

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -154,6 +154,7 @@
         <!-- TODO Jesse: get the user bio -->
             <div id="about-container">
                 <p>
+                    I frequently conducted product, interior, and outdoor photography while working in industrial design. I'm currently focusing on capturing humans' relationship with infrastructure through photo and <a href="https://vimeo.com/danandrewtaylor">video</a>.
                 </p>
             </div>
         <!-- {/if} -->
